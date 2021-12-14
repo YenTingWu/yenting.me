a0a2dd8186fa48fcf388644b0bd04e177c894e8a
YenTingWu <a9600125a@gmail.com>
2021-12-14 14:48:05 +0800

    Refactor chakra-next-image

--- components/UI/ChakraNextImage.tsx
+++ components/UI/ChakraNextImage.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { chakra } from '@chakra-ui/system';
-import NextImage from 'next/image';
+import { Flex } from '@chakra-ui/layout';
+import type { FlexProps } from '@chakra-ui/layout';
+import NextImage, { ImageProps, ImageLoaderProps } from 'next/image';
 
-export const ChakraNextImage = chakra(NextImage, {
+const ChakraNextUnwrappedImage = chakra(NextImage, {
   shouldForwardProp: (prop) =>
     [
       'width',
@@ -11,10 +14,53 @@ export const ChakraNextImage = chakra(NextImage, {
       'quality',
       'placeholder',
       'blurDataURL',
-      'loader',
-      'onClick',
-      'loading',
     ].includes(prop),
 });
+
+const myLoader = (resolverProps: ImageLoaderProps): string => {
+  return `${resolverProps.src}?w=${resolverProps.width}&q=${resolverProps.quality}`;
+};
+
+type Props = FlexProps &
+  ImageProps & {
+    width: string | number;
+    height: string | number;
+  };
+
+export const ChakraNextImage: React.FC<Props> = ({
+  src,
+  width,
+  height,
+  alt,
+  quality,
+  blurDataURL,
+  ...rest
+}) => {
+  delete rest.loader;
+  return (
+    <Flex
+      pos="relative"
+      cursor="pointer"
+      className="group"
+      overflow="hidden"
+      {...rest}
+    >
+      <ChakraNextUnwrappedImage
+        w="auto"
+        h="auto"
+        loader={myLoader}
+        width={width}
+        quality={quality}
+        height={height}
+        placeholder="blur"
+        objectFit="cover"
+        blurDataURL={blurDataURL}
+        src={src}
+        alt={alt}
+        transition="all 0.2s"
+      />
+    </Flex>
+  );
+};
 
 export * from 'next/image';
