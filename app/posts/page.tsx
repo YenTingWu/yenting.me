3b8f69d19cf386c5dcd783fbdca37879a1296b7e
ytw <a9600125a@gmail.com>
2023-12-21 13:25:03 +0800

    feet: reversed-chronological order blog posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,16 +1,24 @@
 import { getPosts } from '@db/blogs';
 import { clsx } from 'clsx';
+import { DateTime } from 'luxon';
 import { Header } from '@components/header';
 import { PostListItem } from '@components/post-list-item';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Posts',
-  description: 'My posts list',
+  description: 'The list of all posts on my blog.',
 };
 
 export default async function Posts() {
-  const posts = await getPosts();
+  const posts = await getPosts().sort((a, b) =>
+    DateTime.fromISO(a.metadata.publishedAt) >
+    DateTime.fromISO(b.metadata.publishedAt)
+      ? -1
+      : 1
+  );
+
+  posts;
 
   return (
     <div
@@ -38,7 +46,9 @@ export default async function Posts() {
         <div className={clsx('w-full', 'mt-5')}>
           <h1 className={clsx('text-4xl', 'font-bold')}>Posts</h1>
         </div>
-        <ul className={clsx('w-full', 'my-12', '[&_*:not(:first-child)]:mt-3')}>
+        <ul
+          className={clsx('w-full', 'my-12', '[&>li:not(:first-child)]:mt-3')}
+        >
           {posts.map((post) => {
             return (
               <PostListItem
